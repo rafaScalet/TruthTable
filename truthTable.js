ff2eb807d40b6f17077e1464692ffe21d72cbe10
muriisantuss <muriisantuss@gmail.com>
2024-04-14 13:39:44 -0300

    finish css

--- truthTable.js
+++ truthTable.js
@@ -20,22 +20,29 @@ function generateTruthTable(proposition, operation) {
     }
     table.push(row);
   }
-
   // Create table header
+  const tableHead = document.createElement("thead");
   const headerRow = document.createElement("tr");
   alphabet.slice(0, proposition).forEach((variable) => {
     const th = document.createElement("th");
     th.textContent = variable;
+    th.setAttribute("scope", "col");
     headerRow.appendChild(th);
   });
 
   const resultHeader = document.createElement("th");
-  resultHeader.textContent = "Result";
+
+  if (operation === "AND") {
+    resultHeader.textContent = "p^q";
+  } else if (operation === "OR") {
+    resultHeader.textContent = "p v q";
+  }
+  resultHeader.setAttribute("scope", "col");
   headerRow.appendChild(resultHeader);
-  addTable.innerHTML = ""; // Limpa a tabela antes de adicionar uma nova
+  tableHead.appendChild(headerRow);
 
-  addTable.appendChild(headerRow);
-
+  // Create table body
+  const tableBody = document.createElement("tbody");
   table.forEach((row) => {
     const tr = document.createElement("tr");
     row.slice(0, proposition).forEach((value) => {
@@ -44,17 +51,27 @@ function generateTruthTable(proposition, operation) {
       tr.appendChild(td);
     });
     const resultCell = document.createElement("td");
-    resultCell.textContent = row[row.length - 1]; // Último valor é o resultado
+    resultCell.textContent = row[row.length - 1];
     tr.appendChild(resultCell);
-    addTable.appendChild(tr);
+    tableBody.appendChild(tr);
   });
+
+  // Clear the table before adding a new one
+  addTable.innerHTML = "";
+  // Append the head and body to the table
+  tableHead.classList.add("table-dark");
+  addTable.appendChild(tableHead);
+  addTable.appendChild(tableBody);
 
   return table;
 }
 
 function displayTruthTable(truthTable, operation) {
   const headerTruthTable = document.getElementById("header");
-  const msg = operation === "AND" ? "tabela verdade conjunção" : "tabela verdade disjunção";
+  const msg =
+    operation === "AND"
+      ? "tabela verdade conjunção"
+      : "tabela verdade disjunção";
   headerTruthTable.textContent = msg;
 
   truthTable.forEach((row) => {
@@ -63,10 +80,10 @@ function displayTruthTable(truthTable, operation) {
 }
 let proposition = 2;
 function updateProposition() {
-    const updatePropositionInput = document.getElementById('change');
-    proposition = parseInt(updatePropositionInput.value);
-    truthTable = generateTruthTable(proposition, operation); 
-    displayTruthTable(truthTable, operation);
+  const updatePropositionInput = document.getElementById("change");
+  proposition = parseInt(updatePropositionInput.value);
+  truthTable = generateTruthTable(proposition, operation);
+  displayTruthTable(truthTable, operation);
 }
 
 let operation = "AND";
@@ -74,15 +91,19 @@ let truthTable = generateTruthTable(proposition, operation);
 displayTruthTable(truthTable, operation);
 
 const conjunction = document.getElementById("AND");
-conjunction.addEventListener("click", function() {
+conjunction.addEventListener("click", function () {
+  disjunction.className = "btn btn-light btn-sm";
+  conjunction.className = "btn btn-dark btn-sm";
   operation = "AND";
-  truthTable = generateTruthTable(proposition, operation); 
-  displayTruthTable(truthTable, operation); 
+  truthTable = generateTruthTable(proposition, operation);
+  displayTruthTable(truthTable, operation);
 });
 
 const disjunction = document.getElementById("OR");
-disjunction.addEventListener("click", function() {
+disjunction.addEventListener("click", function () {
+  conjunction.className = "btn btn-light btn-sm";
+  disjunction.className = "btn btn-dark btn-sm";
   operation = "OR";
-  truthTable = generateTruthTable(proposition, operation); 
+  truthTable = generateTruthTable(proposition, operation);
   displayTruthTable(truthTable, operation);
 });
